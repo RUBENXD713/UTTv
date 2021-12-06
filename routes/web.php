5d6cb8fb056c87f526dd017ed6c80c38ec14908e
RUBENXD713 <19170045@uttcampus.edu.mx>
2021-12-06 17:44:56 -0600

    Cambios en las rutas API/WEB

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//metodos para Usuario
+
+Route::middleware('auth:sanctum')->delete('LogOut','usuarioController@LogOut');
+Route::middleware('auth:sanctum')->get('Perfil','usuarioController@index');
+
+Route::post('Registro','usuarioController@Registro');
+Route::post('Login','usuarioController@LogIn');
+
+Route::put('Actualizar','usuarioController@actualizar')->middleware('admin');
+
+
+//metodos para videos
+
+Route::middleware('auth:sanctum')->get('index','videoController@getVideos');
+
+Route::middleware('auth:sanctum')->get('video','videoController@video');
+
+Route::middleware('auth:sanctum')->post('nuevo','videoController@nuevo');
+
+Route::middleware('auth:sanctum')->put('like','videoController@likeVideo');
+
+Route::middleware('auth:sanctum')->put('dislike','videoController@disLikeVideo');
+
+Route::middleware('auth:sanctum')->get('buscador','videoController@buscador');
